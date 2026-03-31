a7c0686f6e61cbd17fc8ae860b5351fac01fdd2a
gitcoder-aman <amankumar93578@gmail.com>
2026-03-31 17:48:28 +0530

    initial commit

---
+++ src/main/java/com/tech/talkmate_ai/TalkmateAiApplication.java
@@ -0,0 +1,13 @@
+package com.tech.talkmate_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkmateAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkmateAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/talkmate_ai/TalkmateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.talkmate_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkmateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
